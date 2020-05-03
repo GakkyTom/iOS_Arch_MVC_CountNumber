cc7b90addf3371b3340034fde98576ca630b13ef
GakkyTom <itagaki.tomoya@eversense.co.jp>
2020-05-03 18:15:28 +0900

    Initial Commit

---
+++ MVCPracticeTests/MVCPracticeTests.swift
@@ -0,0 +1,34 @@
+//
+//  MVCPracticeTests.swift
+//  MVCPracticeTests
+//
+//  Created by 板垣智也 on 2020/05/03.
+//  Copyright © 2020 板垣智也. All rights reserved.
+//
+
+import XCTest
+@testable import MVCPractice
+
+class MVCPracticeTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
